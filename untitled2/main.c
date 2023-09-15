70aaead62be96743ec7fc6f2600a7f0e8a508fdb
askhta <144684309+askhta@users.noreply.github.com>
2023-09-15 20:49:31 +0700

    baitap

---
+++ untitled2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    float d = 123.123456;
+    printf("float 123.123456: %e", d);
+    char fullname[100];
+    gets(fullname);
+    printf("fullname: %s\n", fullname);
+    char ch, name[40];
+    printf("Please enter the data\n");
+    scanf("%d %f %c %s", &a, &d,&ch,name);
+    printf("\n The values accepted ar: %d,%f,%c,%s", a , d,ch,name);
+    return 0;
+}
